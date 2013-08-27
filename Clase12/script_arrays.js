8e38656b0ee7f4665967e0415eed7339baf4016e
RubenAvilaH <rubenavilah@homecs.co>
2013-08-27 14:01:49 -0500

    Clase12

---
+++ Clase12/script_arrays.js
@@ -0,0 +1,13 @@
+var dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+var diaSeleccionado = dias[0];
+var otroDia = dias[2];
+
+alert(diaSeleccionado);
+alert(otroDia);
+
+// Array > 12 meses > alert();
+
+var pastillaRoja = true;
+var pastillaAzul = false;
+
+alert(pastillaAzul);
---
+++ Clase12/script_variables.js
@@ -0,0 +1,37 @@
+alert("Alert llamado desde Script1.js");
+
+var numero_1 = 2;
+var numero_2 = 4;
+
+var resultado = numero_1 + numero_2;
+alert(resultado);
+
+var numero_1;
+var _letras;
+var $$$total;
+var $_numero_2$;
+
+// NO USAR!
+// var 1_numero; Empieza por un numero
+// var numero;1233; Contiene ;
+
+var iva = 16; // Variable Entero (INT)
+var total = 54.69; //Variable Decimal (FLOAT)
+
+var mensajes = "Hola!"; //Variable de Cadena de Texto (STRING)
+var producto = 'Coca-Cola'; //Variable de Cadena de Texto (STRING)
+
+var texto = "Esta es una 'frase celebre' de un editor";
+var texto_2 = 'Esta es una "frase celebre" \n de un editor';
+alert(texto_2);
+
+// Strings con caracteres de Escape
+var texto1 = "Una frase con \"comillas dobles\" dentro";
+var texto2 = 'Una frase con \'comillas sencillas\' dentro';
+
+var contenido = "Hola! \n Una frase con \"comillas dobles\" \n y sencillas"
+alert(contenido);
+
+
+
+
